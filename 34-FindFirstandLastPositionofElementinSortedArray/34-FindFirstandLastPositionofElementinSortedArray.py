754fd0705a85b24df9e6bdf97a446ec84e1190df
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-03-19 15:09:53 -0400

    File updated at 3/19/2025, 3:09:55 PM

--- 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.py
+++ 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.py
@@ -1,35 +1,60 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        if len(nums) == 0:
+        """
+        if logn, then bts? and its sorted
+        """
+        if nums == []:
             return [-1, -1]
         if len(nums) == 1:
-            if nums[0] == target:
-                return [0, 0]
+            return [0,0] if nums[0] == target else [-1, -1]
+        left = 0
+        right = len(nums) - 1
+        # index = -1 
+        # while left <= right:
+        #     mid = (left + right) // 2
+        #     if nums[mid] < target:
+        #         left = mid + 1
+        #     elif nums[mid] > target:
+        #         right = mid - 1
+        #     else:
+        #         # == target
+        #         index = mid
+        #         break
+        # # print(index)
+        # if index == -1:
+        #     return [-1, -1]
+        found = False
+        left = 0
+        right = len(nums) - 1
+        minLeft = right
+        while left <= right:
+            mid = (left + right) // 2
+            # print(left, mid, right, minLeft, nums)
+            if nums[mid] < target:
+                left = mid + 1
+            elif nums[mid] > target:
+                right = mid - 1
+            elif nums[mid] == target:
+                minLeft = min(minLeft, mid)
+                right = mid - 1
+                found = True
             else:
-                return [-1, -1]
-        
-        l, r = 0, len(nums) - 1
-        
-        while l <= r:
-            mid = l + (r - l) // 2
-            
-            if nums[mid] == target:
-                l = mid - 1
-                r = mid + 1
-                while l >= 0 and nums[l] == target:
-                    l -= 1
-                l += 1
-                
-                while r < len(nums) and nums[r] == target:
-                    r += 1
-                r -= 1
-                
-                return [l, r]
-            
-            elif nums[mid] < target:
-                l = mid + 1
-            
+                print("error")
+        if not found:
+            return [-1, -1]
+        left = minLeft
+        right = len(nums) - 1
+        maxRight = minLeft
+        while left <= right:
+            mid = (left + right) // 2
+            # print(left, mid, right, maxRight, nums)
+            if nums[mid] > target:
+                right = mid - 1
+            elif nums[mid] == target:
+                maxRight = max(maxRight, mid)
+                left = mid + 1
             else:
-                r = mid - 1
+                print("error")
+        return [minLeft, maxRight] 
+
         
-        return [-1, -1]

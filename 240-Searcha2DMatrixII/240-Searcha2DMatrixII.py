ebe8f3aaf2e996d14fb5a7927184c0acfd472fdd
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-03-19 18:59:01 -0400

    File updated at 3/19/2025, 6:59:03 PM

--- 240-Searcha2DMatrixII/240-Searcha2DMatrixII.py
+++ 240-Searcha2DMatrixII/240-Searcha2DMatrixII.py
@@ -9,10 +9,16 @@ class Solution:
 
         while c >= 0 and r <= m-1:
             if matrix[r][c] == target:
+                # found
                 return True
             if matrix[r][c] > target:
+                # current larger than target, 
+                # then target must be on the left / left-bottom side of current
                 c -= 1
             else:
+                # current smaller than target,
+                # then targer must be below current.
+                # Could be just below it, or left-below it.
                 r += 1
         
         return False

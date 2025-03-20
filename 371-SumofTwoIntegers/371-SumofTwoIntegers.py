c8b72286b1c8d9bfc678075f2f612ab11eccbcd3
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-03-20 14:41:20 -0400

    File updated at 3/20/2025, 2:41:22 PM

--- 371-SumofTwoIntegers/371-SumofTwoIntegers.py
+++ 371-SumofTwoIntegers/371-SumofTwoIntegers.py
@@ -1,22 +1,15 @@
 class Solution:
-    def evalRPN(self, tokens: List[str]) -> int:
-        st = list()
+    def majorityElement(self, nums: List[int]) -> int:
+        count = 0
+        candidate = 0
 
-        for i in tokens:
-            if i in '+-*/':
-                num2 = st.pop()
-                num1 = st.pop()
-                if i == '+':
-                    num3 = num1 + num2
-                elif i == '-':
-                    num3 = num1 - num2
-                elif i == '*':
-                    num3 = num1 * num2
-                else:
-                    num3 = int(num1 / num2)
-                st.append(num3)
-                #print(num1, i, num2, '=', num3)
+        for num in nums:
+            if count == 0:
+                candidate = num
+            
+            if candidate == num:
+                count += 1
             else:
-                num3 = int(i)
-                st.append(num3)
-        return st[-1]
+                count -=1
+        
+        return candidate

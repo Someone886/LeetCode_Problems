31fe939667bf3ed1447517f46b5d441db3d49ed2
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-04-04 22:27:42 -0400

    File updated at 4/4/2025, 10:27:40 PM

--- 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.py
+++ 33-SearchinRotatedSortedArray/33-SearchinRotatedSortedArray.py
@@ -1,18 +1,28 @@
+# Last updated: 4/4/2025, 10:27:40 PM
 class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l = 0
+        r = len(nums) - 1
 
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        m = len(matrix)
-        n = len(matrix[0])
-        
-        r = 0
-        c = n - 1
+        while l <= r:
+            middle = (l + r) // 2
+            
+            # print(nums[l], nums[middle], nums[r])
 
-        while c >= 0 and r <= m-1:
-            if matrix[r][c] == target:
-                return True
-            if matrix[r][c] > target:
-                c -= 1
+            if nums[middle] == target:
+                return middle
+            
+            if nums[middle] < nums[r]:
+                if nums[middle] < target <= nums[r]:
+                    l = middle + 1
+                else:
+                    r = middle - 1
+            
             else:
-                r += 1
+                if nums[l] <= target < nums[middle]:
+                    r = middle - 1
+                else:
+                    l = middle + 1
         
-        return False
+        return -1
+        

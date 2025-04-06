0719a75814125a5b663f7800bc1c3d23a99cd1fb
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-04-06 00:59:56 -0400

    File updated at 4/6/2025, 12:59:56 AM

--- 2-AddTwoNumbers/2-AddTwoNumbers.py
+++ 2-AddTwoNumbers/2-AddTwoNumbers.py
@@ -1,41 +1,62 @@
+# Last updated: 4/6/2025, 12:59:56 AM
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        ans_head = ListNode()
-        curr = ans_head
+        carry_over = 0
+
+        curr_1 = l1
+        curr_2 = l2
+        dummy = ListNode()
+        prev = dummy
+
+        while curr_1 != None and curr_2 != None:
+            new_node = ListNode(curr_1.val + curr_2.val + carry_over)
+            carry_over = 0
+
+            if new_node.val >= 10:
+                carry_over = 1
+                new_node.val -= 10
+            
+            prev.next = new_node
+            
+            prev = prev.next
+            curr_1 = curr_1.next
+            curr_2 = curr_2.next
         
-        add_extra = 0
+        while curr_1 != None:
+            new_node = ListNode(curr_1.val + carry_over)
+            carry_over = 0
+
+            if new_node.val >= 10:
+                carry_over = 1
+                new_node.val -= 10
+
+            prev.next = new_node
+
+            prev = prev.next
+            curr_1 = curr_1.next
         
-        l1_head = l1
-        l2_head = l2
+        while curr_2 != None:
+            new_node = ListNode(curr_2.val + carry_over)
+            carry_over = 0
+
+            if new_node.val >= 10:
+                carry_over = 1
+                new_node.val -= 10
+            
+            prev.next = new_node
+
+            prev = prev.next
+            curr_2 = curr_2.next
         
-        while l1_head != None or l2_head != None:
-            node_sum = 0
-            
-            if l1_head != None:
-                node_sum += l1_head.val
-                l1_head = l1_head.next
-            
-            if l2_head != None:
-                node_sum += l2_head.val
-                l2_head = l2_head.next
-            
-            node_sum += add_extra
-            
-            add_extra = node_sum // 10            
-            node_sum -= (add_extra * 10) 
-            
-            ans_node = ListNode(node_sum)
-            curr.next = ans_node
-            
-            curr = ans_node
+        if carry_over == 1:
+            new_node = ListNode(carry_over)
+            carry_over = 0
+            prev.next = new_node
         
-        if add_extra:
-            ans_node = ListNode(add_extra)
-            curr.next = ans_node
-        
-        return ans_head.next
+        return dummy.next

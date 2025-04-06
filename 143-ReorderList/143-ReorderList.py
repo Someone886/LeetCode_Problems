eb0f4be6cf3b87c80ea610b6164dceeed8942eca
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-04-05 20:57:17 -0400

    Create file

---
+++ 143-ReorderList/143-ReorderList.py
@@ -0,0 +1,52 @@
+# Last updated: 4/5/2025, 8:57:16 PM
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+
+        def reverseList(head: Optional[ListNode]):
+            prev, curr = None, head
+
+            while curr:
+                temp = curr.next
+                curr.next = prev
+                prev = curr
+                curr = temp
+                
+            return prev
+            
+        if head == None:
+            return
+        
+        slow, fast = head, head
+        prev_slow = None
+
+        while fast and fast.next != None:
+            prev_slow = slow
+            slow = slow.next
+            fast = fast.next.next
+        
+        if prev_slow == None:
+            return
+        
+        prev_slow.next = None
+
+        l1 = head
+        l2 = slow
+        
+        l2 = reverseList(l2)
+
+        while l1 != None:
+            temp_1 = l1.next
+            temp_2 = l2.next
+
+            l1.next = l2
+            if temp_1 != None:
+                l2.next = temp_1
+
+            l1 = temp_1
+            l2 = temp_2

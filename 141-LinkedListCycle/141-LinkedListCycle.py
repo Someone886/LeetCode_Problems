bf1afda4bb89173137ace0b679aa9bf048e15fb2
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-04-05 20:55:20 -0400

    File updated at 4/5/2025, 8:55:20 PM

--- 141-LinkedListCycle/141-LinkedListCycle.py
+++ 141-LinkedListCycle/141-LinkedListCycle.py
@@ -1,4 +1,4 @@
-# Last updated: 4/5/2025, 5:05:56 PM
+# Last updated: 4/5/2025, 8:55:20 PM
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -6,12 +6,47 @@
 #         self.next = next
 
 class Solution:
-    def hasCycle(self, head: Optional[ListNode]) -> bool:
-        slow, fast = head, head
+    def reorderList(self, head: Optional[ListNode]) -> None:
 
-        while fast and fast.next:
+        def reverseList(head: Optional[ListNode]):
+            prev, curr = None, head
+
+            while curr:
+                temp = curr.next
+                curr.next = prev
+                prev = curr
+                curr = temp
+                
+            return prev
+            
+        if head == None:
+            return
+        
+        slow, fast = head, head
+        prev_slow = None
+
+        while fast and fast.next != None:
+            prev_slow = slow
             slow = slow.next
             fast = fast.next.next
-            if slow == fast:
-                return True
-        return False
+        
+        if prev_slow == None:
+            return
+        
+        prev_slow.next = None
+
+        l1 = head
+        l2 = slow
+        
+        l2 = reverseList(l2)
+
+        while l1 != None:
+            temp_1 = l1.next
+            temp_2 = l2.next
+
+            l1.next = l2
+            if temp_1 != None:
+                l2.next = temp_1
+
+            l1 = temp_1
+            l2 = temp_2

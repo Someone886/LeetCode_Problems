08f4f08537b02850a92bac4cc9e9e440e81b2c3d
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-04-07 16:07:34 -0400

    Create file

---
+++ 703-KthLargestElementinaStream/703-KthLargestElementinaStream.py
@@ -0,0 +1,20 @@
+# Last updated: 4/7/2025, 4:07:32 PM
+import heapq
+
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.heap = nums
+        heapq.heapify(self.heap)
+
+        while len(self.heap) > k:
+            heapq.heappop(self.heap)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.heap, val)
+
+        if len(self.heap) > self.k:
+            heapq.heappop(self.heap)
+        
+        return self.heap[0]

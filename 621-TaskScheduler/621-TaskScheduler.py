6f064b89b643990f88a46243c01f7eaddbe242aa
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-04-08 21:32:16 -0400

    File updated at 4/8/2025, 9:32:15 PM

--- 621-TaskScheduler/621-TaskScheduler.py
+++ 621-TaskScheduler/621-TaskScheduler.py
@@ -1,72 +1,25 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+# Last updated: 4/8/2025, 9:32:15 PM
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        counts = Counter(tasks)
+        neg = [-cnt for cnt in counts.values()]
+        heapq.heapify(neg)
+        pause = deque()
+        time = 0
 
-class Codec:
-
-    def serialize(self, root):
-        """Encodes a tree to a single string.
-        
-        :type root: TreeNode
-        :rtype: str
-        """
-        ans = []
-        
-        if root == None:
-            return str(ans)
-        
-        q = [root]
-        
-        while len(q) != 0:
-            node = q.pop(0)
-            if node != None:
-                ans.append(node.val)
-                q.append(node.left)
-                q.append(node.right)
-            else:
-                ans.append(None)
-        
-        return str(ans)
+        while neg or pause:
+            time += 1
             
-        
+            if neg:
+                max_count = heapq.heappop(neg)
+                max_count += 1
+                if max_count < 0:
+                    pause.append((max_count, time + n))
+            
+            else:
+                time = pause[0][1]
 
-    def deserialize(self, data):
-        """Decodes your encoded data to tree.
-        
-        :type data: str
-        :rtype: TreeNode
-        """
-        data = eval(data)
-        
-        if len(data) == 0:
-            return None
-        
-        root = TreeNode(data.pop(0))
-        q = [root]
-        
-        while len(q) != 0:
-            node = q.pop(0)
+            while pause and pause[0][1] == time:
+                heapq.heappush(neg, pause.popleft()[0])
 
-            if node != None:
-                left = data.pop(0)
-                if left != None:
-                    left_node = TreeNode(left)
-                    node.left = left_node
-                    q.append(left_node)
-                
-                right = data.pop(0)
-                if right != None:
-                    right_node = TreeNode(right)
-                    node.right = right_node
-                    q.append(right_node)
-        
-        return root
-        
-
-# Your Codec object will be instantiated and called as such:
-# ser = Codec()
-# deser = Codec()
-# ans = deser.deserialize(ser.serialize(root))
+        return time   

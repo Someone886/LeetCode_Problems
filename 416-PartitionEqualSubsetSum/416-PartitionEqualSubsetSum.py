0c0b16d2df25bcd5e6678d5a3a7e6d3e7347e95e
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-04-14 15:14:19 -0400

    File updated at 4/14/2025, 3:14:18 PM

--- 416-PartitionEqualSubsetSum/416-PartitionEqualSubsetSum.py
+++ 416-PartitionEqualSubsetSum/416-PartitionEqualSubsetSum.py
@@ -1,23 +1,28 @@
-# Last updated: 4/13/2025, 4:29:05 PM
+# Last updated: 4/14/2025, 3:14:18 PM
 class Solution:
-    def canPartition(self, nums: List[int]) -> bool:
-        summation = sum(nums)
-        if summation % 2 == 1:
-            return False
-        
-        dp = set()
-        dp.add(0)
-        target = summation / 2
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        max_area = 0
+        n = len(grid)
+        m = len(grid[0])
 
-        for num in nums:
-            next_dp = set()
-            for s in dp:
-                if s + num == target:
-                    return True
-                next_dp.add(s)
-                if s + num < target:
-                    next_dp.add(s + num)
-            dp = next_dp
+        def visit(r, c):
+            if r < 0 or r >= n or c < 0 or c >= m:
+                return 0
+            
+            if grid[r][c] == 0 or grid[r][c] == -1:
+                return 0
+            
+            grid[r][c] = -1
+            return 1 + visit(r + 1, c) + \
+                        visit(r - 1, c) + \
+                        visit(r, c + 1) + \
+                        visit(r, c - 1)
+
+        for r in range(n):
+            for c in range(m):
+                if grid[r][c] == 1:
+                    curr_area = visit(r, c)
+                    if curr_area > max_area:
+                        max_area = curr_area
         
-        return False
-                
+        return max_area

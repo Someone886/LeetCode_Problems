79fa80891272f103a4b0fd0a005728e644bf2073
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-04-14 15:32:58 -0400

    File updated at 4/14/2025, 3:32:57 PM

--- 695-MaxAreaofIsland/695-MaxAreaofIsland.py
+++ 695-MaxAreaofIsland/695-MaxAreaofIsland.py
@@ -1,28 +1,39 @@
-# Last updated: 4/14/2025, 3:14:40 PM
+# Last updated: 4/14/2025, 3:32:57 PM
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+from typing import Optional
 class Solution:
-    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        max_area = 0
-        n = len(grid)
-        m = len(grid[0])
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if node == None:
+            return None
 
-        def visit(r, c):
-            if r < 0 or r >= n or c < 0 or c >= m:
-                return 0
-            
-            if grid[r][c] == 0 or grid[r][c] == -1:
-                return 0
-            
-            grid[r][c] = -1
-            return 1 + visit(r + 1, c) + \
-                        visit(r - 1, c) + \
-                        visit(r, c + 1) + \
-                        visit(r, c - 1)
+        old_to_new = {}
 
-        for r in range(n):
-            for c in range(m):
-                if grid[r][c] == 1:
-                    curr_area = visit(r, c)
-                    if curr_area > max_area:
-                        max_area = curr_area
+        def dfs(node):
+            if node in old_to_new:
+                return None
+            
+            node_copy = Node(node.val)
+            old_to_new[node] = node_copy
+
+            neighbors_copy = []
+            for neighbor in node.neighbors:
+                neighbor_copy = dfs(neighbor)
+                if neighbor_copy == None:
+                    neighbor_copy = old_to_new[neighbor]
+                else:
+                    old_to_new[neighbor] = neighbor_copy
+                neighbors_copy.append(neighbor_copy)
+            node_copy.neighbors = neighbors_copy
         
-        return max_area
+            return node_copy
+        
+        dfs(node)
+        return old_to_new[node]
+        

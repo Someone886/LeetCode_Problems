f33a60ebddbe5042c3269f2531734ed1e8a10691
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-04-15 01:46:58 -0400

    Create file

---
+++ 207-CourseSchedule/207-CourseSchedule.py
@@ -0,0 +1,32 @@
+# Last updated: 4/15/2025, 1:46:58 AM
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        pre_map = defaultdict(list)
+
+        for crs, pre in prerequisites:
+            pre_map[crs].append(pre)
+        
+        already_visited = {}
+        # if crs: true, then already visited and processed -> if seen again, then a loop
+        # if crs: false, then its prerequisites are being visited -> if seen again, then pause
+
+        def dfs(crs):
+            if crs in already_visited:
+                return already_visited[crs]
+            
+            already_visited[crs] = False
+
+            for pre in pre_map[crs]:
+                if not dfs(pre):
+                    return False
+
+            already_visited[crs] = True
+
+            return True
+        
+        for crs in range(numCourses):
+            if not dfs(crs):
+                return False
+        
+        return True
+        

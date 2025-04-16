be49dd8961262fd5e8d027fbf6128d821d266a46
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-04-16 19:35:19 -0400

    File updated at 4/16/2025, 7:35:18 PM

--- 518-CoinChangeII/518-CoinChangeII.py
+++ 518-CoinChangeII/518-CoinChangeII.py
@@ -1,26 +1,16 @@
-# Last updated: 4/16/2025, 7:22:05 PM
+# Last updated: 4/16/2025, 7:35:18 PM
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         n = len(coins)
-        dp = {}
+        coins.sort()
+        dp = [[0] * (amount + 1) for _ in range(n + 1)]
 
-        def dfs(index, total):
-            if index >= n:
-                return 0
-            
-            if total > amount:
-                return 0
-            
-            if total == amount:
-                return 1
-
-            if (index, total) in dp:
-                return dp[(index, total)]
-            
-            dp[(index, total)] = dfs(index, total + coins[index]) + \
-                                 dfs(index + 1, total)
-            
-            return dp[(index, total)]
+        for i in range(n + 1):
+            dp[i][-1] = 1
         
-        ans = dfs(0, 0)
-        return ans
+        for i in range(n - 1, -1, -1):
+            for total in range(amount, -1, -1):
+                if total <= amount - coins[i]:
+                    dp[i][total] = dp[i + 1][total] + dp[i][total + coins[i]]
+        
+        return dp[0][0]

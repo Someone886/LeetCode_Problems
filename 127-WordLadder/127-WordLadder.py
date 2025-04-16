fd3395fc3825f88849b71e74b31d7e6090958e0d
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-04-16 08:14:34 -0400

    Create file

---
+++ 127-WordLadder/127-WordLadder.py
@@ -0,0 +1,36 @@
+# Last updated: 4/16/2025, 8:14:33 AM
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        if endWord not in wordList:
+            return 0
+        
+        neighbors = defaultdict(list)
+        wordList.append(beginWord)
+
+        for word in wordList:
+            for j in range(len(word)):
+                pattern = word[:j] + "*" + word[j + 1:]
+                neighbors[pattern].append(word)
+        
+        visited = set([beginWord])
+        q = deque([beginWord])
+        diff = 1
+
+        while q:
+            diff += 1
+
+            for i in range(len(q)):
+                next_word = q.popleft()
+
+                for j in range(len(next_word)):
+                    pattern = next_word[:j] + "*" + next_word[j + 1:]
+
+                    for neighbor in neighbors[pattern]:
+                        if neighbor == endWord:
+                            return diff
+
+                        if neighbor not in visited:
+                            visited.add(neighbor)
+                            q.append(neighbor)
+
+        return 0

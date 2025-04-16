8e3c24d6531430df47d1de44c57bbf9dd035b47d
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-04-16 14:00:11 -0400

    File updated at 4/16/2025, 2:00:10 PM

--- 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.py
+++ 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.py
@@ -1,25 +1,16 @@
-# Last updated: 4/16/2025, 1:53:31 PM
+# Last updated: 4/16/2025, 2:00:10 PM
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         len_1 = len(text1)
         len_2 = len(text2)
 
-        dp = [[-1] * len_1 for _ in range(len_2)]
+        dp = [[0] * (len_2 + 1) for _ in range(len_1 + 1)]
 
-        def helper(index_1, index_2):
-            if index_1 == len_1 or index_2 == len_2:
-                return 0
-            
-            if dp[index_2][index_1] != -1:
-                return dp[index_2][index_1]
-            
-            if_same = 1 if text1[index_1] == text2[index_2] else 0
-
-            if if_same:
-                dp[index_2][index_1] = 1 + helper(index_1 + 1, index_2 + 1)
-            else:
-                dp[index_2][index_1] = max(helper(index_1 + 1, index_2), helper(index_1, index_2 + 1))
-            
-            return dp[index_2][index_1]
-
-        return helper(0, 0)
+        for i in range(1, len_1 + 1):
+            for j in range(1, len_2 + 1):
+                if text1[i - 1] == text2[j - 1]:
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        
+        return dp[len_1][len_2]

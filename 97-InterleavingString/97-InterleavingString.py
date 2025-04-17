427b5cf1311c96c921427059ed9139c15bd478d3
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-04-16 21:15:24 -0400

    Update 97-InterleavingString.py

--- 97-InterleavingString/97-InterleavingString.py
+++ 97-InterleavingString/97-InterleavingString.py
@@ -1,23 +1,29 @@
-# Last updated: 4/16/2025, 9:10:11 PM
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        if len(s1) + len(s2) != len(s3):
+        if len(s3) != len(s1) + len(s2):
             return False
-
+        
         dp = {}
-        def dfs(i, j, k):
+
+        # index i and j are the chars to be munipulated
+        def helper(i, j):
+            k = i + j
+
             if k == len(s3):
-                return (i == len(s1)) and (j == len(s2))
+                return True
+            
             if (i, j) in dp:
                 return dp[(i, j)]
             
-            res = False
+            ans = False
             if i < len(s1) and s1[i] == s3[k]:
-                res = dfs(i + 1, j, k + 1)
-            if not res and j < len(s2) and s2[j] == s3[k]:
-                res = dfs(i, j + 1, k + 1)
+                ans = helper(i + 1, j)
             
-            dp[(i, j)] = res
-            return res
+            if not ans:
+                if j < len(s2) and s2[j] == s3[k]:
+                    ans = helper(i, j + 1)
+            
+            dp[(i, j)] = ans
+            return ans
         
-        return dfs(0, 0, 0)
+        return helper(0, 0)

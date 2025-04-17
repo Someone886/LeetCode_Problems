9474b692baffdb985226d66fc217f2029ba6ffe1
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-04-17 15:47:00 -0400

    File updated at 4/17/2025, 3:47:00 PM

--- 57-InsertInterval/57-InsertInterval.py
+++ 57-InsertInterval/57-InsertInterval.py
@@ -1,37 +1,36 @@
-# Last updated: 4/17/2025, 3:37:15 PM
+# Last updated: 4/17/2025, 3:47:00 PM
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         if len(intervals) == 0:
             return [newInterval]
 
-        target_index = -1
-        if intervals[0][0] >= newInterval[0]:
-            target_index = -1
-        
-        elif intervals[len(intervals) - 1][0] <= newInterval[0]:
-            target_index = len(intervals) - 1
-        
-        else:
-            for i in range(len(intervals) - 1):
-                if intervals[i][0] <= newInterval[0] <= intervals[i + 1][0]:
-                    target_index = i
-                    break
+        n = len(intervals)
+        target = newInterval[0]
+        l, r = 0, n - 1
+        found_index = -1
 
-        intervals = intervals[:target_index + 1] + [newInterval] + intervals[target_index + 1:]
-        
-        if target_index == -1:
-            target_index = 0
+        while l <= r:
+            mid = (l + r) // 2
+            if intervals[mid][0] == target:
+                found_index = mid + 1
+                break
 
-        j = target_index
-
-        while j < len(intervals) - 1:
-            curr = intervals[j]
-            nxt = intervals[j + 1]
-
-            if curr[1] >= nxt[0]:
-                intervals[j] = [curr[0], max(curr[1], nxt[1])]
-                intervals.pop(j + 1)
+            elif intervals[mid][0] < target:
+                l = mid + 1
+            
             else:
-                j += 1
+                r = mid - 1
+        
+        if found_index == -1:
+            found_index = r + 1
 
-        return intervals
+        intervals = intervals[:found_index] + [newInterval] + intervals[found_index:]
+
+        res = []
+        for interval in intervals:
+            if not res or res[-1][1] < interval[0]:
+                res.append(interval)
+            else:
+                res[-1][1] = max(res[-1][1], interval[1])
+        
+        return res

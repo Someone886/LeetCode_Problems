7d6dc5844331bee38cfb9b2f3f69cb76f1194cfd
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-04-19 16:51:03 -0400

    File updated at 4/19/2025, 4:51:00 PM

--- 73-SetMatrixZeroes/73-SetMatrixZeroes.py
+++ 73-SetMatrixZeroes/73-SetMatrixZeroes.py
@@ -1,32 +1,35 @@
-class Solution(object):
-    def setZeroes(self, matrix):
+# Last updated: 4/19/2025, 4:51:00 PM
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
         """
-        :type matrix: List[List[int]]
-        :rtype: None Do not return anything, modify matrix in-place instead.
+        Do not return anything, modify matrix in-place instead.
         """
-        first_row = 1
-        
-        m = len(matrix)
-        n = len(matrix[0])
-        
-        for i in range(m):
-            for j in range(n):
+        first_column_zero = False
+
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
                 if matrix[i][j] == 0:
-                    matrix[0][j] = 0
-                    
-                    if i > 0:
+                    if j == 0:
+                        first_column_zero = True
                         matrix[i][0] = 0
                     else:
-                        first_row = 0
+                        matrix[0][j] = 0
+                        matrix[i][0] = 0
         
-        for i in range(1, m):
+        for i in range(1, len(matrix)):
             if matrix[i][0] == 0:
-                matrix[i] = [0] * n
-        
-        for j in range(0, n):
-            if matrix[0][j] == 0:
-                for i in range(1, m):
+                for j in range(1, len(matrix[0])):
                     matrix[i][j] = 0
         
-        if first_row == 0:
-            matrix[0] = [0] * n
+        for i in range(1, len(matrix[0])):
+            if matrix[0][i] == 0:
+                for j in range(1, len(matrix)):
+                    matrix[j][i] = 0
+        
+        if matrix[0][0] == 0:
+            for i in range(0, len(matrix[0])):
+                matrix[0][i] = 0
+        
+        if first_column_zero:
+            for i in range(0, len(matrix)):
+                matrix[i][0] = 0

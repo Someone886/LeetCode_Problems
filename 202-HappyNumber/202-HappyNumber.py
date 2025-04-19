58c1ca6e35d66dd5d4f519a14903d119d0a6be4e
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-04-19 17:01:22 -0400

    File updated at 4/19/2025, 5:01:19 PM

--- 202-HappyNumber/202-HappyNumber.py
+++ 202-HappyNumber/202-HappyNumber.py
@@ -1,15 +1,18 @@
-# Last updated: 4/19/2025, 4:59:18 PM
+# Last updated: 4/19/2025, 5:01:19 PM
 class Solution:
     def isHappy(self, n: int) -> bool:
         slow, fast = n, n
-        first = True
+        fast = self.sumOfSquares(fast)
+        fast = self.sumOfSquares(fast)
 
-        while first or slow != fast:
-            first = False
+        while slow != fast:
+            if fast == 1:
+                return True
             
             fast = self.sumOfSquares(fast)
             fast = self.sumOfSquares(fast)
             slow = self.sumOfSquares(slow)
+
         return True if fast == 1 else False
     
     def sumOfSquares(self, n: int) -> int:

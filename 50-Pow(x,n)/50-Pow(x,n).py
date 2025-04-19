4551c572ab3807e8d9eb7c49f75326fd50740976
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-04-19 17:28:14 -0400

    File updated at 4/19/2025, 5:28:11 PM

--- 50-Pow(x,n)/50-Pow(x,n).py
+++ 50-Pow(x,n)/50-Pow(x,n).py
@@ -1,39 +1,19 @@
+# Last updated: 4/19/2025, 5:28:11 PM
 class Solution:
     def myPow(self, x: float, n: int) -> float:
+        if x == 0:
+            return 0
+
         if n == 0:
             return 1
         
-        if n == 1:
-            return x
-        
-        if n == -1:
-            return 1/x
-
-        if x == 1:
-            return x
-        
-        if x == -1:
-            if n % 2 == 0:
-                return 1
-            else:
-                return -1
-        
-        negative = False
-        if n < 0:
-            x = 1/x
-            negative = True
-            n = -n-1
-        
         ans = 1
-        temp = x
+        power = abs(n)
 
-        while (n != 0):
-            if n % 2 == 1:
-                ans *= temp
-            temp *= temp
-            n = n // 2
-
-        if negative:
-            ans = ans * x
-
-        return ans
+        while power:
+            if power % 2 == 1:
+                ans *= x
+            x *= x
+            power = power // 2
+        
+        return ans if n > 0 else 1 / ans

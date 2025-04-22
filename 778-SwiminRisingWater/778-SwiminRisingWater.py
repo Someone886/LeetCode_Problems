6f80de92749fcac44f3fc9dc584ad0639ef430b5
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-04-22 04:53:27 -0400

    File updated at 4/22/2025, 4:53:26 AM

--- 778-SwiminRisingWater/778-SwiminRisingWater.py
+++ 778-SwiminRisingWater/778-SwiminRisingWater.py
@@ -1,4 +1,4 @@
-# Last updated: 4/22/2025, 4:24:15 AM
+# Last updated: 4/22/2025, 4:53:26 AM
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
         n = len(grid)
@@ -7,16 +7,19 @@ class Solution:
         heap = [(grid[0][0], 0, 0)]
         heapq.heapify(heap)
         visited = set()
-
         max_water = 0
         dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        
         while (n - 1, n - 1) not in visited:
-            water, x, y = heapq.heappop(heap)
+            max_water, x, y = heapq.heappop(heap)
+            
+            if x == y == n - 1:
+                return max_water
+
             if (x, y) in visited:
                 continue
-            
-            visited.add((x, y))
-            max_water = max(water, max_water)
+            else:
+                visited.add((x, y))
 
             for dx, dy in dirs:
                 if 0 <= x + dx < n and 0 <= y + dy < n and (x + dx, y + dy) not in visited:

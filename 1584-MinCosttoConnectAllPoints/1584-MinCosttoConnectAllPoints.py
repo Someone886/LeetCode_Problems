fc1b898ed292fe86fbfbffecafd554c8e7570680
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-04-22 02:48:51 -0400

    File updated at 4/22/2025, 2:48:50 AM

--- 1584-MinCosttoConnectAllPoints/1584-MinCosttoConnectAllPoints.py
+++ 1584-MinCosttoConnectAllPoints/1584-MinCosttoConnectAllPoints.py
@@ -1,50 +1,26 @@
-# Last updated: 4/22/2025, 2:37:13 AM
+# Last updated: 4/22/2025, 2:48:50 AM
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        edges = []
-        recorded_points = []
+        total = 0
+        heap = [(0, 0)]
+        heapq.heapify(heap)
+        visited = set()
 
-        for point in points:
-            for i in range(len(recorded_points)):
-                recorded_point = recorded_points[i]
-                dist = abs(point[0] - recorded_point[0]) + abs(point[1] - recorded_point[1])
-                edges.append((dist, i, len(recorded_points)))
-            recorded_points.append(point)
-        
-        edges.sort()
-
-        n = len(recorded_points)
-        parents = [i for i in range(n)]
-        size = [1] * n
-
-        def find(n):
-            parent = parents[n]
-            while parent != parents[parent]:
-                parents[parent] = parents[parents[parent]]
-                parent = parents[parent]
-            
-            return parent
-        
-        def union(u, v):
-            parent_1, parent_2 = find(u), find(v)
-            if parent_1 == parent_2:
-                return False
-            
-            if size[parent_1] > size[parent_2]:
-                size[parent_1] += size[parent_2]
-                parents[parent_2] = parent_1
-            else:
-                size[parent_2] += size[parent_1]
-                parents[parent_1] = parent_2
-            
-            return True
-        
-        total_length = 0
-
-        for dist, u, v in edges:
-            if not union(u, v):
+        while len(visited) < len(points):
+            dist, pt = heapq.heappop(heap)
+            if pt in visited:
                 continue
-            else:
-                total_length += dist
+            
+            visited.add(pt)
+            total += dist
+            x, y = points[pt][0], points[pt][1]
 
-        return total_length
+            for i in range(len(points)):
+                if i in visited:
+                    continue
+                
+                x_j, y_j = points[i][0], points[i][1]
+                next_dist = abs(x - x_j) + abs(y - y_j)
+                heapq.heappush(heap, (next_dist, i))
+        
+        return total

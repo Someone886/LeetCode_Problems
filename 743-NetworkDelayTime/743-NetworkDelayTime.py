175f2ca7fc9d62b27818ad6878e745c3d0485950
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-04-22 04:53:01 -0400

    File updated at 4/22/2025, 4:53:00 AM

--- 743-NetworkDelayTime/743-NetworkDelayTime.py
+++ 743-NetworkDelayTime/743-NetworkDelayTime.py
@@ -1,31 +1,29 @@
-# Last updated: 4/22/2025, 4:51:53 AM
+# Last updated: 4/22/2025, 4:53:00 AM
 class Solution:
-    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        edges = collections.defaultdict(list)
-
-        for u, v, t in times:
-            edges[u].append((v, t))
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+    
+        # heap point = grid[x][y], x, y
+        heap = [(grid[0][0], 0, 0)]
+        heapq.heapify(heap)
+        visited = set()
+        max_water = 0
+        dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
         
-        # min_heap keeps len(path) to the node i from the start node k
-        min_heap = [(0, k)]
-        visit = set()
-
-        # everytime adds the shortest node from the start node k to the explored graph
-        # after exploring node i, add its neighbors to the heap
-        # ~ BFS
-        while min_heap:
-            l, node = heapq.heappop(min_heap)
+        while (n - 1, n - 1) not in visited:
+            max_water, x, y = heapq.heappop(heap)
             
-            if node in visit:
+            if x == y == n - 1:
+                return max_water
+
+            if (x, y) in visited:
                 continue
             else:
-                visit.add(node)
-            
-            if len(visit) == n:
-                return l
+                visited.add((x, y))
 
-            for neighbor, weight in edges[node]:
-                if neighbor not in visit:
-                    heapq.heappush(min_heap, (l + weight, neighbor))
+            for dx, dy in dirs:
+                if 0 <= x + dx < n and 0 <= y + dy < n and (x + dx, y + dy) not in visited:
+                    heapq.heappush(heap, (max(max_water, grid[x + dx][y + dy]), x + dx, y + dy))
         
-        return -1
+        return max_water
+        

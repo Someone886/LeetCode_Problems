f47db7a5da4c255ae4dc80a1ecdd262a475751e4
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-04-22 04:51:54 -0400

    File updated at 4/22/2025, 4:51:53 AM

--- 743-NetworkDelayTime/743-NetworkDelayTime.py
+++ 743-NetworkDelayTime/743-NetworkDelayTime.py
@@ -1,4 +1,4 @@
-# Last updated: 4/21/2025, 1:36:54 AM
+# Last updated: 4/22/2025, 4:51:53 AM
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         edges = collections.defaultdict(list)
@@ -9,22 +9,23 @@ class Solution:
         # min_heap keeps len(path) to the node i from the start node k
         min_heap = [(0, k)]
         visit = set()
-        t = 0   # result to be returned -> total length
 
         # everytime adds the shortest node from the start node k to the explored graph
         # after exploring node i, add its neighbors to the heap
         # ~ BFS
         while min_heap:
-            w_1, n_1 = heapq.heappop(min_heap)
-            if n_1 in visit:
+            l, node = heapq.heappop(min_heap)
+            
+            if node in visit:
                 continue
             else:
-                visit.add(n_1)
+                visit.add(node)
             
-            t = w_1
+            if len(visit) == n:
+                return l
 
-            for neighbor, weight in edges[n_1]:
+            for neighbor, weight in edges[node]:
                 if neighbor not in visit:
-                    heapq.heappush(min_heap, (t + weight, neighbor))
+                    heapq.heappush(min_heap, (l + weight, neighbor))
         
-        return t if len(visit) == n else -1
+        return -1

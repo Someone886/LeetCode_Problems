9a6e050ea4f36bc41d5029b069f84653b93477c9
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-04-22 18:44:27 -0400

    File updated at 4/22/2025, 6:44:27 PM

--- 787-CheapestFlightsWithinKStops/787-CheapestFlightsWithinKStops.py
+++ 787-CheapestFlightsWithinKStops/787-CheapestFlightsWithinKStops.py
@@ -1,31 +1,19 @@
-# Last updated: 4/22/2025, 6:39:28 PM
+# Last updated: 4/22/2025, 6:44:27 PM
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
         # Bellman-Ford algorithm that can intuitively handle the k stops condition.
-        cost_cnt = [{} for _ in range(n)]
-        cost_cnt[src][-1] = 0
+        prices = [float("inf")] * n
+        prices[src] = 0
 
-        for _ in range(n - 1):
+        for i in range(k + 1):
+            # modify new_prices 
+            # so that only destinations within i stops from src (which are reachable) are updated
+            new_prices = prices.copy()
+
             for start, end, price in flights:
-                if not cost_cnt[start]:
-                    continue
-                
-                start_cost_cnt = cost_cnt[start]
-                for stop_cnt, cost in start_cost_cnt.items():
-                    new_stop_cnt = stop_cnt + 1
-                    new_cost = cost + price
-                    
-                    if new_stop_cnt <= k:
-                        if new_stop_cnt not in cost_cnt[end]:
-                            cost_cnt[end][new_stop_cnt] = new_cost
-                        elif new_cost < cost_cnt[end][new_stop_cnt]:
-                            cost_cnt[end][new_stop_cnt] = new_cost
+                if prices[start] + price < new_prices[end]:
+                    new_prices[end] = prices[start] + price
+
+            prices = new_prices
         
-        if not cost_cnt[dst]:
-            return -1
-        
-        min_cost = min(cost_cnt[dst].values())
-        if min_cost != float('inf'):
-            return min_cost
-        else:
-            return -1
+        return -1 if prices[dst] == float("inf") else prices[dst]

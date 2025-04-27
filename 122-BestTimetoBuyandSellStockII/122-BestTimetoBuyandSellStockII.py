8aedb4ce9100b081f137f0481222cc18d77085de
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-04-26 22:43:19 -0400

    File updated at 4/26/2025, 10:43:18 PM

--- 122-BestTimetoBuyandSellStockII/122-BestTimetoBuyandSellStockII.py
+++ 122-BestTimetoBuyandSellStockII/122-BestTimetoBuyandSellStockII.py
@@ -1,23 +1,12 @@
-# Last updated: 4/26/2025, 10:40:33 PM
+# Last updated: 4/26/2025, 10:43:18 PM
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        dp = {}
-        n = len(prices)
+        profit = 0
+        held_price = prices[0]
 
-        def helper(day, holding):
-            if day >= n:
-                return 0
-
-            if (day, holding) in dp:
-                return dp[(day, holding)]
-            
-            if holding:
-                profit = max(prices[day] + helper(day + 1, False), helper(day + 1, True))
-                dp[(day, holding)] = profit
-            else:
-                profit = max(-prices[day] + helper(day + 1, True), helper(day + 1, False))
-                dp[(day, holding)] = profit
-            
-            return dp[(day, holding)]
-        
-        return helper(0, False)
+        for i in range(1, len(prices)):
+            if prices[i] >= held_price:
+                profit += prices[i] - held_price
+                
+            held_price = prices[i]
+        return profit

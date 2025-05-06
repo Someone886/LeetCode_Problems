2100613573a3c36d9747aae5288a503e48128d65
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-05-05 21:22:44 -0400

    Create file

---
+++ 377-CombinationSumIV/377-CombinationSumIV.py
@@ -0,0 +1,24 @@
+# Last updated: 5/5/2025, 9:22:41 PM
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        dp = [-1] * (target + 1)
+
+        def dfs(left):
+            if left == 0:
+                return 1
+            
+            if left < 0:
+                return 0
+            
+            if dp[left] != -1:
+                return dp[left]
+            
+            ways = 0
+            for num in nums:
+                ways += dfs(left - num)
+            
+            dp[left] = ways
+            return dp[left]
+        
+        ans = dfs(target)
+        return ans

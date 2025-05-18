714535fce6f65e9b108c43bf9f7551cafe8fd32a
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-05-18 01:35:07 -0400

    Create file

---
+++ 953-VerifyinganAlienDictionary/953-VerifyinganAlienDictionary.py
@@ -0,0 +1,18 @@
+# Last updated: 5/18/2025, 1:35:07 AM
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        order_index = {c: i for i, c in enumerate(order)}
+        
+        for i in range(len(words) - 1):
+            w1, w2 = words[i], words[i + 1]
+            
+            for j in range(len(w1)):
+                if w1.startswith(w2) and len(w1) > len(w2):
+                    return False
+
+                if w1[j] != w2[j]:
+                    if order_index[w1[j]] > order_index[w2[j]]:
+                        return False
+                    break
+        
+        return True

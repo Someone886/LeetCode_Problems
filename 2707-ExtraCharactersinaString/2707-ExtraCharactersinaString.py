647bf2dbe8b9c13e49fb927463f2c6f087f14839
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-05-31 04:42:12 -0400

    File updated at 5/31/2025, 4:42:12 AM

--- 2707-ExtraCharactersinaString/2707-ExtraCharactersinaString.py
+++ 2707-ExtraCharactersinaString/2707-ExtraCharactersinaString.py
@@ -1,4 +1,49 @@
-# Last updated: 5/31/2025, 4:37:59 AM
+# Last updated: 5/31/2025, 4:42:12 AM
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word):
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        curr.end = True
+
+class Solution:
+    def minExtraChar(self, s: str, dictionary: List[str]) -> int:
+        dp = {len(s): 0}
+        trie = Trie()
+
+        for w in dictionary:
+            trie.addWord(w)
+        
+        def dfs(i):
+            if i in dp:
+                return dp[i]
+
+            res = 1 + dfs(i + 1)
+            curr = trie.root
+
+            for j in range(i, len(s)):
+                if s[j] not in curr.children:
+                    break
+                curr = curr.children[s[j]]
+                if curr.end:
+                    res = min(res, dfs(j + 1))
+
+            dp[i] = res
+            return res
+
+        return dfs(0)
+
+'''
 class Solution:
     def minExtraChar(self, s: str, dictionary: List[str]) -> int:
         words = set(dictionary)
@@ -19,3 +64,4 @@ class Solution:
             return res
         
         return dfs(0)
+'''

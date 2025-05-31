7024452371eaaed5718b38aa5eaceba44a13bf4f
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-05-31 04:38:00 -0400

    Create 2707-ExtraCharactersinaString

---
+++ 2707-ExtraCharactersinaString/2707-ExtraCharactersinaString.py
@@ -0,0 +1,21 @@
+# Last updated: 5/31/2025, 4:37:59 AM
+class Solution:
+    def minExtraChar(self, s: str, dictionary: List[str]) -> int:
+        words = set(dictionary)
+        dp = {len(s): 0}
+
+        def dfs(i):
+            if i in dp:
+                return dp[i]
+
+            res = len(s) - i
+
+            for j in range(i, len(s)):
+                if s[i:j + 1] in words:
+                    res = min(res, dfs(j + 1))
+            res = min(res, 1 + dfs(i + 1))
+
+            dp[i] = res
+            return res
+        
+        return dfs(0)

4ff40aab387a015d1f0eb819c05fc77f9e202bc3
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-06-22 18:50:57 +0000

    Create 937-OnlineStockSpan

---
+++ 937-OnlineStockSpan/937-OnlineStockSpan.py
@@ -0,0 +1,29 @@
+# Last updated: 6/22/2025, 2:50:56 PM
+class StockSpanner:
+
+    def __init__(self):
+        self.stock_stack = []
+        self.span = []
+
+    def next(self, price: int) -> int:
+        if not self.stock_stack:
+            self.stock_stack.append(price)
+            self.span.append(1)
+            return 1
+        else:
+            total_span = 1
+            
+            while self.stock_stack and self.stock_stack[-1] <= price:
+                last_price = self.stock_stack.pop()
+                last_span = self.span.pop()
+                total_span += last_span
+            
+            self.stock_stack.append(price)
+            self.span.append(total_span)
+
+            return total_span
+
+
+# Your StockSpanner object will be instantiated and called as such:
+# obj = StockSpanner()
+# param_1 = obj.next(price)

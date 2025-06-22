16d6a6a7d8d3c947b11d9e2f7bffdac930f0e82e
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-06-22 18:53:30 +0000

    Create 38-CountAndSay

---
+++ 38-CountAndSay/38-CountAndSay.py
@@ -0,0 +1,37 @@
+# Last updated: 6/22/2025, 2:53:29 PM
+class Solution(object):
+    def countAndSay(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        if n == 1:
+            return "1"
+        
+        prev = self.countAndSay(n - 1)
+
+        left = 0
+        right = 0
+        count = 0
+
+        ans = ""
+        n = len(prev)
+
+        while left < n and right < n:
+            c_left = prev[left]
+            c_right = prev[right]
+
+            if c_left == c_right:
+                count += 1
+                right += 1
+            else:
+                ans += str(count) + c_left
+                left = right
+                count = 1
+                right += 1
+
+        c_left = prev[left]
+        ans += str(count) + c_left
+
+        return ans
+        

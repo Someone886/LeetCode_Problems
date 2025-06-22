e3dadb1e5a0b34320ffac6c49755047119caca2f
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-06-22 18:50:59 +0000

    Create 909-StoneGame

---
+++ 909-StoneGame/909-StoneGame.py
@@ -0,0 +1,23 @@
+# Last updated: 6/22/2025, 2:50:58 PM
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        dp = {}
+        n = len(piles)
+
+        def helper(front_index, back_index):
+            if front_index == back_index:
+                return piles[front_index]
+            
+            if (front_index, back_index) in dp:
+                return dp[(front_index, back_index)]
+            
+            max_diff = max(piles[front_index] - helper(front_index + 1, back_index),\
+                           piles[back_index] - helper(front_index, back_index - 1))
+            
+            dp[(front_index, back_index)] = max_diff
+            return max_diff
+        
+        ans = helper(0, n - 1)
+        return True if ans > 0 else False
+
+# actually Alice always wins -> return True lol

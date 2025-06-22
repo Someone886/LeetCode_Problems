a6b113bc929c615b42835d027b83d0c59397941c
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-06-22 18:51:33 +0000

    Create 309-BestTimeToBuyAndSellStockWithCooldown

---
+++ 309-BestTimeToBuyAndSellStockWithCooldown/309-BestTimeToBuyAndSellStockWithCooldown.py
@@ -0,0 +1,29 @@
+# Last updated: 6/22/2025, 2:51:32 PM
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:        
+        # state: buy or sell
+        # if buy, move time to i+1
+        # if sell, move time to i+2
+
+        dp = {} # key = (i, can_buy), val = max_profit
+
+        def dfs(i, can_buy):
+            if i >= len(prices):
+                return 0
+            
+            if (i, can_buy) in dp:
+                return dp[(i, can_buy)]
+            
+            if can_buy:
+                buy = -prices[i] + dfs(i + 1, False)
+                cooldown = dfs(i + 1, True)
+                dp[(i, can_buy)] = max(buy, cooldown)
+            else:
+                sell = prices[i] + dfs(i + 2, True)
+                cooldown = dfs(i + 1, False)
+                dp[(i, can_buy)] = max(sell, cooldown)
+            
+            return dp[(i, can_buy)]
+        
+        return dfs(0, True)
+        

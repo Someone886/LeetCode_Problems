e8568407440d24aa8eba5cadaff22bed9248e465
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-06-22 18:51:17 +0000

    Create 518-CoinChangeIi

---
+++ 518-CoinChangeIi/518-CoinChangeIi.py
@@ -0,0 +1,16 @@
+# Last updated: 6/22/2025, 2:51:16 PM
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        n = len(coins)
+        coins.sort()
+        dp = [[0] * (amount + 1) for _ in range(n + 1)]
+
+        for i in range(n + 1):
+            dp[i][-1] = 1
+        
+        for i in range(n - 1, -1, -1):
+            for total in range(amount, -1, -1):
+                if total <= amount - coins[i]:
+                    dp[i][total] = dp[i + 1][total] + dp[i][total + coins[i]]
+        
+        return dp[0][0]

2e4188c356dfa6253ab9f37ea81cf1d4ae6b3647
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-06-22 18:51:06 +0000

    Create 789-KthLargestElementInAStream

---
+++ 789-KthLargestElementInAStream/789-KthLargestElementInAStream.py
@@ -0,0 +1,20 @@
+# Last updated: 6/22/2025, 2:51:05 PM
+import heapq
+
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.heap = nums
+        heapq.heapify(self.heap)
+
+        while len(self.heap) > k:
+            heapq.heappop(self.heap)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.heap, val)
+
+        if len(self.heap) > self.k:
+            heapq.heappop(self.heap)
+        
+        return self.heap[0]

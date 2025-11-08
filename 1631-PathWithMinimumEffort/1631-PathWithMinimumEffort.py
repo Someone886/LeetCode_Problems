b9f2407e0acad70b14c23d616bbbb6966c713311
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-11-07 23:04:10 -0500

    File updated at 11/7/2025, 11:04:09 PM

--- 1631-PathWithMinimumEffort/1631-PathWithMinimumEffort.py
+++ 1631-PathWithMinimumEffort/1631-PathWithMinimumEffort.py
@@ -1,26 +1,28 @@
-# Last updated: 4/22/2025, 4:46:49 AM
+# Last updated: 11/7/2025, 11:04:09 PM
 class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
-        rows, cols = len(heights), len(heights[0])
-        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-        minHeap = [(0, 0, 0)] 
+        min_eff = 0
         visited = set()
-        
-        while minHeap:
-            effort, x, y = heappop(minHeap)
+        n = len(heights)
+        m = len(heights[0])
 
-            if (x, y) in visited:
+        # min effort to reach an index, r, c
+        q = [(0, 0, 0)]
+
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        while (n-1, m-1) not in visited:
+            eff, r, c = heapq.heappop(q)
+            if (r, c) in visited:
                 continue
-            else:
-                visited.add((x,y))
-
-            if x == rows - 1 and y == cols - 1:
-                return effort
             
-            for dx, dy in directions:
-                nx, ny = x + dx, y + dy
-                
-                if 0 <= nx < rows and 0 <= ny < cols and (nx, ny) not in visited:
-                    new_effort = max(effort, abs(heights[x][y] - heights[nx][ny]))
-                    heappush(minHeap, (new_effort, nx, ny))
+            visited.add((r, c))
+            min_eff = eff
+
+            for (dr, dc) in directions:
+                if 0 <= r + dr < n and 0 <= c + dc < m:
+                    if (r + dr, c + dc) not in visited:
+                        neighbor_height = heights[r + dr][c + dc]
+                        heapq.heappush(q, (max(min_eff, abs(neighbor_height - heights[r][c])), r + dr, c + dc))
         
+        return min_eff

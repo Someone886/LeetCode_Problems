494bda715dfc96082759d39f0b0b26a113ba936b
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-11-13 18:27:06 -0500

    File updated at 11/13/2025, 6:27:05 PM

--- 135-Candy/135-Candy.py
+++ 135-Candy/135-Candy.py
@@ -1,21 +1,44 @@
-# Last updated: 11/13/2025, 5:58:58 PM
+# Last updated: 11/13/2025, 6:27:05 PM
 class Solution:
     def candy(self, ratings: List[int]) -> int:
+        # first pass from left to right: 
+        #   child at index i has more candies than its left neighbor if child i has a higher ranking
+        # second pass from right to left:
+        #   child at index i has more candies than its right neighbor if child i has a higher ranking
+
         n = len(ratings)
         candies = [1] * n
-
-        q = [(ranking, index) for index, ranking in enumerate(ratings)]
-        heapq.heapify(q)
-
-        while q:
-            child_ranking, child_index = heapq.heappop(q)
-            
-            if child_index - 1 >= 0:
-                if child_ranking < ratings[child_index - 1]:
-                    candies[child_index - 1] = max(candies[child_index - 1], 1 + candies[child_index])
-
-            if child_index + 1 <= n - 1:
-                if child_ranking < ratings[child_index + 1]:
-                    candies[child_index + 1] = max(candies[child_index + 1], 1 + candies[child_index])
         
+        for i in range(1, n):
+            if ratings[i] > ratings[i - 1]:
+                candies[i] = max(candies[i], candies[i - 1] + 1)
+        
+        for i in range(n - 2, -1, -1):
+            if ratings[i] > ratings[i + 1]:
+                candies[i] = max(candies[i], candies[i + 1] + 1)
+
         return sum(candies)
+
+
+# heapq with nlogn:
+
+# class Solution:
+#     def candy(self, ratings: List[int]) -> int:
+#         n = len(ratings)
+#         candies = [1] * n
+
+#         q = [(ranking, index) for index, ranking in enumerate(ratings)]
+#         heapq.heapify(q)
+
+#         while q:
+#             child_ranking, child_index = heapq.heappop(q)
+            
+#             if child_index - 1 >= 0:
+#                 if child_ranking < ratings[child_index - 1]:
+#                     candies[child_index - 1] = max(candies[child_index - 1], 1 + candies[child_index])
+
+#             if child_index + 1 <= n - 1:
+#                 if child_ranking < ratings[child_index + 1]:
+#                     candies[child_index + 1] = max(candies[child_index + 1], 1 + candies[child_index])
+        
+#         return sum(candies)

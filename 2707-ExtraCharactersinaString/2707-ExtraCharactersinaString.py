8811a4759c61f6fb6d23c4f277c18a0faa4b5c5a
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-11-16 06:41:17 -0500

    File updated at 11/16/2025, 6:41:17 AM

--- 2707-ExtraCharactersinaString/2707-ExtraCharactersinaString.py
+++ 2707-ExtraCharactersinaString/2707-ExtraCharactersinaString.py
@@ -1,67 +1,27 @@
-# Last updated: 5/31/2025, 4:42:12 AM
-class TrieNode:
-    def __init__(self):
-        self.children = {}
-        self.end = False
-
-class Trie:
-    def __init__(self):
-        self.root = TrieNode()
-
-    def addWord(self, word):
-        curr = self.root
-        for c in word:
-            if c not in curr.children:
-                curr.children[c] = TrieNode()
-            curr = curr.children[c]
-        curr.end = True
-
+# Last updated: 11/16/2025, 6:41:17 AM
 class Solution:
     def minExtraChar(self, s: str, dictionary: List[str]) -> int:
-        dp = {len(s): 0}
-        trie = Trie()
+        dp = {}
+        n = len(s)
 
-        for w in dictionary:
-            trie.addWord(w)
-        
-        def dfs(i):
-            if i in dp:
-                return dp[i]
+        def dfs(sub_s, breaking_index):
+            if not sub_s:
+                return 0
+            
+            if breaking_index in dp:
+                return dp[breaking_index]
 
-            res = 1 + dfs(i + 1)
-            curr = trie.root
+            min_left = len(sub_s)
+            for word in dictionary:
+                word_location = sub_s.find(word)
+                if word_location != -1:
+                    leftover = word_location + \
+                        dfs(sub_s[word_location + len(word):], breaking_index + word_location + len(word))
+                    min_left = min(min_left, leftover)
+                
+            dp[breaking_index] = min_left
 
-            for j in range(i, len(s)):
-                if s[j] not in curr.children:
-                    break
-                curr = curr.children[s[j]]
-                if curr.end:
-                    res = min(res, dfs(j + 1))
+            return min_left
 
-            dp[i] = res
-            return res
-
-        return dfs(0)
-
-'''
-class Solution:
-    def minExtraChar(self, s: str, dictionary: List[str]) -> int:
-        words = set(dictionary)
-        dp = {len(s): 0}
-
-        def dfs(i):
-            if i in dp:
-                return dp[i]
-
-            res = len(s) - i
-
-            for j in range(i, len(s)):
-                if s[i:j + 1] in words:
-                    res = min(res, dfs(j + 1))
-            res = min(res, 1 + dfs(i + 1))
-
-            dp[i] = res
-            return res
-        
-        return dfs(0)
-'''
+        ans = dfs(s, 0)
+        return ans

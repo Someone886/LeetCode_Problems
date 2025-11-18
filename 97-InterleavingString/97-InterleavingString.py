89c77fe8b11b79045042cdc0bd16d8aa17a2b78e
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-11-18 03:59:14 -0500

    File updated at 11/18/2025, 3:59:14 AM

--- 97-InterleavingString/97-InterleavingString.py
+++ 97-InterleavingString/97-InterleavingString.py
@@ -1,29 +1,32 @@
+# Last updated: 11/18/2025, 3:59:14 AM
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
         if len(s3) != len(s1) + len(s2):
             return False
-        
+            
+        len1 = len(s1)
+        len2 = len(s2)
         dp = {}
 
-        # index i and j are the chars to be munipulated
-        def helper(i, j):
-            k = i + j
-
-            if k == len(s3):
-                return True
+        def sub_interleave(index1, index2):
+            if index1 == len1:
+                return s2[index2:] == s3[len1 + index2:]
             
-            if (i, j) in dp:
-                return dp[(i, j)]
+            if index2 == len2:
+                return s1[index1:] == s3[len2 + index1:]
             
-            ans = False
-            if i < len(s1) and s1[i] == s3[k]:
-                ans = helper(i + 1, j)
+            if (index1, index2) in dp:
+                return dp[(index1, index2)]
             
-            if not ans:
-                if j < len(s2) and s2[j] == s3[k]:
-                    ans = helper(i, j + 1)
+            dp[(index1, index2)] = False
+            if s1[index1] == s3[index1 + index2]:
+                dp[(index1, index2)] = sub_interleave(index1 + 1, index2)
             
-            dp[(i, j)] = ans
-            return ans
+            if not dp[(index1, index2)] and s2[index2] == s3[index1 + index2]:
+                dp[(index1, index2)] = sub_interleave(index1, index2 + 1)
+            
+            return dp[(index1, index2)]
         
-        return helper(0, 0)
+        ans = sub_interleave(0, 0)
+        return ans
+            

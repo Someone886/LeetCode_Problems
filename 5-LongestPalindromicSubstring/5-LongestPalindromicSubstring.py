2c3352c6399d7f3e05107fc1bf24930c54bae312
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-11-20 18:02:20 -0500

    File updated at 11/20/2025, 6:02:19 PM

--- 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.py
@@ -1,36 +1,36 @@
-# Last updated: 4/12/2025, 11:02:10 PM
+# Last updated: 11/20/2025, 6:02:19 PM
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        max_len = 1
-        max_interval = [0, 1]
         n = len(s)
+        longest = 1
+        max_left, max_right = 0, 1
 
-        for center in range(0, n - 1):
-            l, r = center - 1, center + 1
-            while l >= 0 and r <= n - 1:
-                if s[l] != s[r]:
-                    break
+        for center in range(0, n-1):
+            left = center - 1
+            right = center + 1
 
-                l -= 1
-                r += 1
+            while left >= 0 and right < n and s[left] == s[right]:
+                left -= 1
+                right += 1
             
-            length = (r-1) - (l+1) + 1
-            if length > max_len:
-                max_len = length
-                max_interval = [l + 1, r]
-            
-            l, r = center, center + 1
-            while l >= 0 and r <= n - 1:
-                if s[l] != s[r]:
-                    break
+            length = right - left - 1
+            if length > longest:
+                longest = length
+                max_left = left + 1
+                max_right = right
 
-                l -= 1
-                r += 1
-            
-            length = (r-1) - (l+1) + 1
-            if length > max_len:
-                max_len = length
-                max_interval = [l + 1, r]
+            if s[center] == s[center + 1]:
+                left = center
+                right = center + 1
+
+                while left >= 0 and right < n and s[left] == s[right]:
+                    left -= 1
+                    right += 1
+                
+                length = right - left - 1
+                if length > longest:
+                    longest = length
+                    max_left = left + 1
+                    max_right = right
         
-        return s[max_interval[0] : max_interval[1]]
-        
+        return s[max_left : max_right]

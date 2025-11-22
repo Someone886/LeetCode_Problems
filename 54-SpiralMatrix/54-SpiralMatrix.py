8c0a4720c1e1ad8458544cf2c3d39768f499570d
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-11-21 22:10:32 -0500

    File updated at 11/21/2025, 10:10:32 PM

--- 54-SpiralMatrix/54-SpiralMatrix.py
+++ 54-SpiralMatrix/54-SpiralMatrix.py
@@ -1,28 +1,30 @@
-# Last updated: 4/19/2025, 3:14:15 PM
+# Last updated: 11/21/2025, 10:10:32 PM
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        l, r = 0, len(matrix[0])
-        top, bottom = 0, len(matrix)
-        res = []
+        top = 0
+        bottom = len(matrix) - 1
+        left = 0
+        right = len(matrix[0]) - 1
 
-        while l < r and top < bottom:
-            for i in range(l, r):
-                res.append(matrix[top][i])
+        ans = []
+
+        while top <= bottom and right >= left:
+            for i in range(left, right + 1):
+                ans.append(matrix[top][i])
             top += 1
 
-            for i in range(top, bottom):
-                res.append(matrix[i][r - 1])
-            r -= 1
-            
-            if not (l < r and top < bottom):
-                break
+            for i in range(top, bottom + 1):
+                ans.append(matrix[i][right])
+            right -= 1
 
-            for i in range(r - 1, l - 1, -1):
-                res.append(matrix[bottom - 1][i])
-            bottom -= 1
-            
-            for i in range(bottom - 1, top - 1, -1):
-                res.append(matrix[i][l])
-            l += 1
+            if top <= bottom:
+                for i in range(right, left - 1, -1):
+                    ans.append(matrix[bottom][i])
+                bottom -= 1
 
-        return res
+            if left <= right:
+                for i in range(bottom, top - 1, -1):
+                    ans.append(matrix[i][left])
+                left += 1
+
+        return ans

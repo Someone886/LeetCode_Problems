f16573956a767725bdbf93eae4eac1c1ed9967d2
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-12-26 18:23:17 -0500

    File updated at 12/26/2025, 6:23:17 PM

--- 22-GenerateParentheses/22-GenerateParentheses.py
+++ 22-GenerateParentheses/22-GenerateParentheses.py
@@ -1,48 +1,28 @@
-class Solution(object):
-    def generateParenthesis(self, n):
-        """
-        :type n: int
-        :rtype: List[str]
-        """
-        ans = []
-        brackets = ['(', ')']
-        
-        if n == 0:
-            return ans
-        
-        def helper(n_left, n_right, string):
-            if n_left == 0 and n_right == 0:
-                ans.append(string)
-                return
-            
-            if n_left == 0:
-                for i in range(n_right):
-                    string += ')'
-                ans.append(string)
-                return
-            
-            if n_left < n_right:
-                string = string + '('
-                helper(n_left - 1, n_right, string)
-                string = string[:-1]
-                
-                string = string + ')'
-                helper(n_left, n_right - 1, string)
-                string = string[:-1]
-            
-            elif n_left == n_right:
-                string = string + '('
-                helper(n_left - 1, n_right, string)
-                string = string[:-1]
-            
-            else:
-                string = string + ')'
-                helper(n_left, n_right-1, string)
-                string = string[:-1]
-                
-            return
-    
-        helper(n, n, '')
-        
-        return ans
-        
+# Last updated: 12/26/2025, 6:23:17 PM
+1class Solution:
+2    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+3        if not s or len(s) == 0:
+4            return True
+5        n = len(s)
+6        dp = {}
+7
+8        def fit_words(index):
+9            if index == n:
+10                return True
+11            
+12            if index in dp:
+13                return dp[index]
+14            
+15            can_fit = False
+16            for word in wordDict:
+17                m = len(word)
+18                if s[index:index+m] == word:
+19                    can_fit = fit_words(index + m)
+20                    if can_fit:
+21                        break
+22            
+23            dp[index] = can_fit
+24            
+25            return dp[index]
+26        
+27        return fit_words(0)

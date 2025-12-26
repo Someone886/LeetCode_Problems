e1f80555c5eb67deaf99d523cfc40357aa1c3ef6
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-12-26 18:58:02 -0500

    File updated at 12/26/2025, 6:58:02 PM

--- 79-WordSearch/79-WordSearch.py
+++ 79-WordSearch/79-WordSearch.py
@@ -1,63 +1,35 @@
-class Solution:
-    def exist(self, board: List[List[str]], word: str) -> bool:
-        
-        m = len(board)
-        n = len(board[0])
-
-        len_word = len(word)
-        
-        def helper(r, c, char, len_string):
-            
-            if len_string == len_word:
-                return 1
-            
-            if len_string > len_word:
-                return 0
-            
-            if r < 0 or r > m - 1 or c < 0 or c > n - 1:
-                return 0
-            
-            letter = board[r][c]
-            
-            if letter != word[len_string]:
-                return 0
-            
-            else:
-                board[r][c] = ''
-                
-                found = helper(r-1, c, letter, len_string + 1)
-                if found:
-                    return 1
-                
-                found = helper(r+1, c, letter, len_string + 1)
-                if found:
-                    return 1
-                
-                found = helper(r, c-1, letter, len_string + 1)
-                if found:
-                    return 1
-                
-                found = helper(r, c+1, letter, len_string + 1)
-                if found:
-                    return 1
-                
-                board[r][c] = letter
-                
-            return 0
-        
-        cnt = Counter(c for row in board for c in row)
-        # This line checks if the board has at least as many occurrences of each character as needed to form the given word.
-        if not cnt >= Counter(word):
-            return False
-
-        # This conditional checks which end of the word is less common on the board and potentially reverses the word.
-        if cnt[word[-1]] < cnt[word[0]]:
-            word = word[::-1]
-
-        for r in range(m):
-            for c in range(n):
-                found = helper(r, c, '', 0)
-                if found:
-                    return True
-                
-        return False
+# Last updated: 12/26/2025, 6:58:02 PM
+1class Solution:
+2    def exist(self, board: List[List[str]], word: str) -> bool:
+3        so_far = []
+4        r, c = len(board), len(board[0])
+5        directions = ((-1, 0), (1, 0), (0, -1), (0, 1))
+6
+7        def search(x, y):
+8            temp = board[x][y]
+9            so_far.append(board[x][y])
+10            board[x][y] = "-1"
+11
+12            if len(so_far) == len(word):
+13                board[x][y] = temp
+14                return True
+15
+16            for dx, dy in directions:
+17                if 0 <= x + dx < r and 0 <= y + dy < c:
+18                    if board[x + dx][y + dy] == word[len(so_far)]:
+19                        ans = search(x+dx, y+dy)
+20                        if ans:
+21                            board[x][y] = temp
+22                            return True
+23            
+24            board[x][y] = temp
+25            so_far.pop()
+26            
+27            return False
+28        
+29        for i in range(r):
+30            for j in range(c):
+31                if board[i][j] == word[0] and search(i, j):
+32                    return True
+33        
+34        return False

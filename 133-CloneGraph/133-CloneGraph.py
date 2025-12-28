3f4e258a7d1006e96e0b1c7f7b85c3beb1267733
Someone886 <77801763+Someone886@users.noreply.github.com>
2025-12-28 02:21:03 -0500

    File updated at 12/28/2025, 2:21:03 AM

--- 133-CloneGraph/133-CloneGraph.py
+++ 133-CloneGraph/133-CloneGraph.py
@@ -1,39 +1,37 @@
-# Last updated: 4/14/2025, 3:33:20 PM
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val = 0, neighbors = None):
-        self.val = val
-        self.neighbors = neighbors if neighbors is not None else []
-"""
-
-from typing import Optional
-class Solution:
-    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        if node == None:
-            return None
-
-        old_to_new = {}
-
-        def dfs(node):
-            if node in old_to_new:
-                return None
-            
-            node_copy = Node(node.val)
-            old_to_new[node] = node_copy
-
-            neighbors_copy = []
-            for neighbor in node.neighbors:
-                neighbor_copy = dfs(neighbor)
-                if neighbor_copy == None:
-                    neighbor_copy = old_to_new[neighbor]
-                else:
-                    old_to_new[neighbor] = neighbor_copy
-                neighbors_copy.append(neighbor_copy)
-            node_copy.neighbors = neighbors_copy
-        
-            return node_copy
-        
-        dfs(node)
-        return old_to_new[node]
-        
+# Last updated: 12/28/2025, 2:21:03 AM
+1"""
+2# Definition for a Node.
+3class Node:
+4    def __init__(self, val = 0, neighbors = None):
+5        self.val = val
+6        self.neighbors = neighbors if neighbors is not None else []
+7"""
+8
+9from typing import Optional
+10class Solution:
+11    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+12        if node == None:
+13            return None
+14
+15        old_to_new = {}
+16
+17        def dfs(node):
+18            # already visited, do not create new copy and mapping
+19            if node in old_to_new:
+20                return old_to_new[node]
+21            
+22            # not visited yet, create new copy and mapping
+23            node_copy = Node(node.val)
+24            old_to_new[node] = node_copy
+25
+26            neighbors_copy = []
+27            for neighbor in node.neighbors:
+28                neighbor_copy = dfs(neighbor)
+29                neighbors_copy.append(neighbor_copy)
+30            node_copy.neighbors = neighbors_copy
+31        
+32            return node_copy
+33        
+34        dfs(node)
+35        return old_to_new[node]
+36        

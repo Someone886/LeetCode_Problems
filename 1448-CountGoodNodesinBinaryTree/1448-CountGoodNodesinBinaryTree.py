acb860773585b7ddc3dc57fcfa4677cef3e73e1c
Someone886 <77801763+Someone886@users.noreply.github.com>
2026-01-11 22:56:11 -0500

    File updated at 1/11/2026, 10:56:11 PM

--- 1448-CountGoodNodesinBinaryTree/1448-CountGoodNodesinBinaryTree.py
+++ 1448-CountGoodNodesinBinaryTree/1448-CountGoodNodesinBinaryTree.py
@@ -1,30 +1,27 @@
-# Last updated: 4/10/2025, 3:19:14 AM
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def goodNodes(self, root: TreeNode) -> int:
-        if root == None:
-            return 0
-        
-        cnt = 0
-
-        def counter(node, max_val):
-            nonlocal cnt
-
-            if node == None:
-                return
-            
-            new_max = max_val
-            if node.val >= max_val:
-                new_max = node.val
-                cnt += 1
-            
-            counter(node.left, new_max)
-            counter(node.right, new_max)
-        
-        counter(root, root.val)
-        return cnt
+# Last updated: 1/11/2026, 10:56:11 PM
+1# Definition for a binary tree node.
+2# class TreeNode:
+3#     def __init__(self, val=0, left=None, right=None):
+4#         self.val = val
+5#         self.left = left
+6#         self.right = right
+7
+8class Solution:
+9    def goodNodes(self, root: TreeNode) -> int:
+10        good_nodes_cnt = 0
+11
+12        def dfs(node, max_so_far):
+13            nonlocal good_nodes_cnt
+14
+15            if node.val >= max_so_far:
+16                good_nodes_cnt += 1
+17            
+18            new_max_so_far = max(node.val, max_so_far)
+19            if node.left:
+20                dfs(node.left, new_max_so_far)
+21            if node.right:
+22                dfs(node.right, new_max_so_far)
+23            
+24        dfs(root, float("-inf"))
+25        return good_nodes_cnt
+26            
